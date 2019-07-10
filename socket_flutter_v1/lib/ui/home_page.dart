5ac1cfd59328aa2e0490dd73f24c967a6b0378aa
vishwesh <vishweshsoni@gmail.com>
2019-07-10 20:14:54 +0530

    added dependancies

--- socket_flutter_v1/lib/ui/home_page.dart
+++ socket_flutter_v1/lib/ui/home_page.dart
@@ -5,10 +5,46 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.orange,
+        backgroundColor: Colors.blueAccent,
+        body: Container(
+
+           alignment: FractionalOffset.center,
+            child: Column(
+              children: <Widget>[
+                MaterialButton(
+                    color: Colors.grey,
+                    child: Text('Room1',style: TextStyle(color: Colors.black),),
+                    onPressed: ()=>{
+
+                       }
+                    ),
+                MaterialButton(
+                    color: Colors.grey,
+                    child: Text('Room2',style: TextStyle(color: Colors.black),),
+                    onPressed: ()=>{
+
+                    }
+                ),
+                MaterialButton(
+                    color: Colors.grey,
+                    child: Text('Room3',style: TextStyle(color: Colors.black),),
+                    onPressed: ()=>{
+
+                    }
+                ),
+              ],
+            ),
+        ),
     );
   }
 }
