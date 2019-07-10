496aa5b080e6d91ed6702c0fd3e29cf247b27af1
vishwesh <vishweshsoni@gmail.com>
2019-07-10 19:43:54 +0530

    Mobile first commit

---
+++ socket_flutter_v1/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:socket_flutter_v1/ui/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
