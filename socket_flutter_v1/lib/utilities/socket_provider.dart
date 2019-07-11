04350c3140cfef8a143960f54cd1d37f1f80fe8c
vishwesh <vishweshsoni@gmail.com>
2019-07-12 00:53:09 +0530

    sfds

--- socket_flutter_v1/lib/utilities/socket_provider.dart
+++ socket_flutter_v1/lib/utilities/socket_provider.dart
@@ -1,13 +1,13 @@
 import 'package:adhara_socket_io/manager.dart';
 import 'package:adhara_socket_io/socket.dart';
 import 'package:flutter/foundation.dart';
-import 'dart:io';
+//import 'dart:io';
+
 import 'package:socket_flutter_plugin/socket_flutter_plugin.dart';
-//import 'package:adhara_socket_io/adhara_socket_io.dart';
+import 'package:adhara_socket_io/adhara_socket_io.dart';
 class SocketProvider with ChangeNotifier{
   bool connect= false;
   connectSocket(String str) async{
-
 
 
       SocketIO socket = await SocketIOManager().createInstance('https://arcane-dusk-93500.herokuapp.com/',enableLogging: true,query: {
